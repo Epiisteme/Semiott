9b0ef02a92cf60be6f20d40ac10e242e6b9fcb58
Gokul Alex <gokulsan@users.noreply.github.com>
2020-09-03 02:12:11 +0530

    Update SequentialHashing.sol

--- SequentialHashing.sol
+++ SequentialHashing.sol
@@ -1,15 +1,17 @@
-pragma solidity ^0.4.7;
+pragma solidity ^0.7.0;
+
 
 contract SequentialHashing {
 
-    function verify(uint256 x,uint256 y) view returns (bool){
+
+    function verify(uint256 x,uint256 y) public view returns (bool){
       uint256 p = 1000000007;
         for (uint count = 0; count < 10^4; count += 1){
             y=modExp(y,2,p);
-            }
-            return x==y;
-         }   
-          
+        }
+        return x==y;
+      }   
+      
     function modExp(uint256 base, uint256 exp, uint256 mod) internal view returns (uint256 result)  {
       result = 1;
       for (uint count = 1; count <= exp; count *= 2) {
@@ -19,9 +21,8 @@ contract SequentialHashing {
         }
       return result;
       }
-   
-    
-    function hash(uint256 x) view returns (uint256 r){
+ 
+    function hash(uint256 x) public view returns (uint256 r){
         uint256 p = 1000000007;
         x=x%p;
         for (uint count1 = 0; count1 < 10^4; count1 ++){
@@ -30,5 +31,4 @@ contract SequentialHashing {
         }
         return x;
       }
-      
     }
